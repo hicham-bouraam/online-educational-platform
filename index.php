2c2b069594d1dad66d7f2650fc88e133e9a6b233
hicham-bouraam <126594361+hicham-bouraam@users.noreply.github.com>
2023-03-08 14:19:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (isset($_SESSION['username'])) {
 }
 
 // Check if the user has submitted the login form
-if (isset($_POST['username']) && isset($_POST['password'])) {
+elseif (isset($_POST['username']) && isset($_POST['password'])) {
     // Validate the username and password here
     if ($_POST['username'] == 'admin' && $_POST['password'] == 'password') {
         // Set the session variable to mark the user as logged in
