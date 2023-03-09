ad881a009afcec940d84cf6cb3f7423016d20e19
hicham-bouraam <126594361+hicham-bouraam@users.noreply.github.com>
2023-03-09 15:49:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -48,14 +48,6 @@ if ($result->num_rows > 0) {
 $conn->close();
 
 ?>
-    <form method="post">
-        <label>Username:</label>
-        <input type="text" name="username"><br>
-
-        <label>Password:</label>
-        <input type="password" name="password"><br>
-
-        <input type="submit" value="Login">
-    </form>
+   
 </body>
 </html>
