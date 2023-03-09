4fe5e3c4dcc842ebfe6d0b2e998c959b64145e5f
hicham-bouraam <126594361+hicham-bouraam@users.noreply.github.com>
2023-03-09 15:29:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,6 @@ $db_user = "jyhgeucacf";
 $db_pass = "A5UB284AV146VV63$";
 $db_name = "online-educational-platform-database";
 
-
-
 // Create a connection to the database
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -15,51 +13,31 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Create a table with a boolean column
-$sql = "CREATE TABLE user_status (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  is_on_webpage TINYINT(1) NOT NULL
-)";
+// Check if there is already a user on the webpage
+$sql = "SELECT is_on_webpage FROM user_status LIMIT 1";
+$result = $conn->query($sql);
 
-if ($conn->query($sql) === TRUE) {
-  echo "Table created successfully";
+if ($result->num_rows > 0) {
+  // There is already a user on the webpage, redirect to a different page
+  header("Location: page1.php");
 } else {
-  echo "Error creating table: " . $conn->error;
-}
+  // There is no user on the webpage, set the boolean variable to true and insert it into the table
+  $is_on_webpage = true;
 
-// Set the boolean variable to true to indicate that the user is on the webpage
-$is_on_webpage = true;
+  $sql = "INSERT INTO user_status (is_on_webpage)
+  VALUES ($is_on_webpage)";
 
-// Insert the boolean variable into the table
-$sql = "INSERT INTO user_status (is_on_webpage)
-VALUES ($is_on_webpage)";
-
-if ($conn->query($sql) === TRUE) {
-  echo "Boolean value inserted successfully";
-} else {
-  echo "Error inserting boolean value: " . $conn->error;
+  if ($conn->query($sql) === TRUE) {
+    // Redirect to a different page
+    header("Location: page2.php");
+  } else {
+    echo "Error inserting boolean value: " . $conn->error;
+  }
 }
 
 // Close the database connection
 $conn->close();
 
-
-// Check if the user has submitted the login form
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    // Validate the username and password here
-    if ($_POST['username'] == 'admin' && $_POST['password'] == 'password') {
-        // Set the session variable to mark the user as logged in
-        $_SESSION['username'] = $_POST['username'];
-
-        // Redirect to the secure page or display a success message
-        header('Location: home.php');
-        exit;
-    } else {
-        // Display an error message
-        $error = 'Invalid username or password';
-    }
-}
-}
 ?>
 
 <!DOCTYPE html>
