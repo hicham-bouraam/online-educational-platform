8b2a269e71c595490a132a790a987402d53a4f92
hicham-bouraam <126594361+hicham-bouraam@users.noreply.github.com>
2023-03-14 12:06:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,52 +1,25 @@
 <?php
-// Database connection parameters
 $servername =  "online-educational-platform-server.mysql.database.azure.com";
 $username = "jyhgeucacf";
 $password = "A5UB284AF146VV63$";
-$dbname = "online-educational-platform-server";;
+$dbname = "online-educational-platform-server";
 
-// Create a connection to the database
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check the connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-
-// Check if there is already a user on the webpage
-$sql = "SELECT is_on_webpage FROM user_status LIMIT 1";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // There is already a user on the webpage, redirect to a different page
-  header("Location: watch.php");
-  exit();
-} else {
-  // There is no user on the webpage, set the boolean variable to true and insert it into the table
-  $is_on_webpage = true;
-
-  $sql = "INSERT INTO user_status (is_on_webpage)
-  VALUES ($is_on_webpage)";
-
-  if ($conn->query($sql) === TRUE) {
-    // User is the first one on the webpage, allow access
-  } else {
-    echo "Error inserting boolean value: " . $conn->error;
-  }
-}
-
-// Close the database connection
-$conn->close();
-
-// Start your HTML content below this point
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>My Webpage</title>
-  </head>
-  <body>
-    <h1>Welcome to my webpage</h1>
-    <p>This is some content on my webpage.</p>
-  </body>
+<head>
+    <title>My Page</title>
+</head>
+<body>
+    <h1>Welcome to my page!</h1>
+    <p>Here's some content for the page.</p>
+</body>
 </html>
+
